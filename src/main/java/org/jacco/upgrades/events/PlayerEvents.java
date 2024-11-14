ceb300cb119fe69b4fb07d2e505ee8f028c50fc9
Jacco <jaccokoridon@gmail.com>
2024-11-14 21:47:55 +0100

    Added PlayerMove event handler

--- src/main/java/org/jacco/upgrades/events/PlayerEvents.java
+++ src/main/java/org/jacco/upgrades/events/PlayerEvents.java
@@ -7,10 +7,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.jacco.upgrades.Upgrades;
 import world.bentobox.bentobox.api.flags.Flag;
+import world.bentobox.bentobox.listeners.JoinLeaveListener;
 
 import java.util.Optional;
 
@@ -45,7 +47,38 @@ public class PlayerEvents implements Listener {
             double locationX = location.getX();
             double locationZ = location.getZ();
 
-            if (Math.abs(centerX - locationX) > 10 || Math.abs(centerZ - locationZ) > 10) {
+            if (Math.abs(centerX - locationX) > addon.getSettings().getIslandRange(i) || Math.abs(centerZ - locationZ) > addon.getSettings().getIslandRange(i)) {
+                event.setCancelled(true);
+            }
+
+        });
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        Location from = event.getFrom();
+        Location to = event.getTo();
+
+        if (!addon.inGameWorld(from.getWorld())) {
+            return;
+        }
+
+        addon.getIslands().getIslandAt(from).ifPresent(i -> {
+
+            if (!i.onIsland(to)) {
+                event.setCancelled(true);
+            }
+
+            Location center = i.getCenter();
+
+            double centerX = center.getX();
+            double centerZ = center.getZ();
+
+            double toX = to.getX();
+            double toZ = to.getZ();
+
+            if (Math.abs(centerX - toX) > (addon.getSettings().getIslandRange(i) + 10) || Math.abs(centerZ - toZ) > (addon.getSettings().getIslandRange(i) + 10)) {
                 event.setCancelled(true);
             }
 
